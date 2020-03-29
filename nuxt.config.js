585d9175cf7450a590d10e14caad232092bde1d2
Qurram <qurram.zaheer@gmail.com>
2020-03-29 11:58:54 +0530

    mobile fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || 'Qurram Zaheer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
